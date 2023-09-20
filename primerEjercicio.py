f948f4e1d4fae4dc674d80757f0ce7932f864b78
Hector Turcios <hectorturcios254@gmail.com>
2023-09-20 16:51:40 -0600

    Agregando seccion 1-2 de la tarea

---
+++ primerEjercicio.py
@@ -0,0 +1,18 @@
+import sys
+
+# Define una variable de cada tipo de primitivo
+numero = 3 #int
+flotante = 3.14 #float
+boleano = True #Boolean
+texto = "Hola mundo" #String
+
+# Concatena a la cadena las otras variables aplicando la conversión correcta para funcionar, guarda el resultado en una variable
+texto = str(numero) + str(flotante) + str(boleano)
+
+# Investiga sobre el límite de los enteros y los flotantes en python y anotar sus descubrimientos como comentarios en el archivo
+
+"""
+
+
+"""
+print(sys.maxsize)
